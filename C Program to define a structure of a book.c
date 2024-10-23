827a6fbb8cea5926a5b534d04f0ec16c6697539e
Terminete <paulmuli878@gmail.com>
2024-10-23 11:00:35 +0300

    Update C Program to define a structure of a book.c

--- C Program to define a structure of a book.c
+++ C Program to define a structure of a book.c
@@ -8,16 +8,16 @@ struct book{
     int publication_year;
     char ISBN[30];
     float price;
-}book1;
+}book1,book2;
 int main(){
     //struct book book1;
     //strcpy(book1.title"Introduction  c programming");
     printf("Enter title of the book: ");
     scanf("%s",&book1.title);
     strcpy(book1.author,"John smith");
-    book1.publication_year=2000;
+    book1.publication_year=2022;
     strcpy(book1.ISBN,"978013110627");
-    book1.price=47.88;
+    book1.price=49.88;
     
     printf("Author: %s \n",book1.author);
     printf("publication_year: %d \n",book1.publication_year);
